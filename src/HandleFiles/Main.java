b52e9b14e1a3919e5a7ae5f7174e105313bb4ff2
yago.esh <yago.esh@gmail.com>
2018-08-08 09:45:21 +0200

    Remove Warnings
    Remove Warnings

--- src/HandleFiles/Main.java
+++ src/HandleFiles/Main.java
@@ -2,22 +2,19 @@ package HandleFiles;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
 
